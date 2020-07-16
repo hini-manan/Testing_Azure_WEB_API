66bb24fbd1e714f3b8da5c4841f9065a45f53d3f
Prabhodhini Lakshmanan <prabhodhini.lakshmanan@gmail.com>
2020-07-16 10:37:44 +0530

    quick update

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Testing_Azure_WEB_API
-A sample testing function exploring the features of Azure WEB API's verbs for HTTP requests (HTTP: GET, PUT, PUSH, DELETE).
+A sample testing function exploring the features of Azure WEB API's -
+verbs for HTTP requests (HTTP: GET, PUT, PUSH, DELETE)
--- interest_calculator/Controllers/InterestController.cs
+++ interest_calculator/Controllers/InterestController.cs
@@ -8,13 +8,14 @@ namespace interest_calculator.Controllers
     [ApiController]
     public class InterestController : ControllerBase
     {
+        // Interest object Class to hold properties needed for calculating interest
         public class Interest
         {
             public int id { get; set; }
             public float principle { get; set; }
             public double interest { get; set; }
         }
-
+        // Calculation onbect Class to calculate the annual and monthly interest for the given principle and interest rate
         public class Calculation
         {
             public int calId { get; set; }
@@ -22,6 +23,7 @@ namespace interest_calculator.Controllers
             public double monInt { get; set; }
         }
 
+        // List holding set of predefined Interest rates and Principles
         List<Interest> int_lst = new List<Interest>()
         {
             new Interest(){id=1, principle=10000, interest=2.5},
@@ -30,7 +32,7 @@ namespace interest_calculator.Controllers
             new Interest(){id=4, principle=55000, interest=5.5},
             new Interest(){id=5, principle=78000, interest=3.6}
         };
-
+        // List to hold set of calculated annual and monthly interests
         List<Calculation> cal_lst = new List<Calculation>();
 
         /// <summary>Calculate interest rate (annual/monthly) for a given principle and interest rate if avaliable</summary>
@@ -44,13 +46,10 @@ namespace interest_calculator.Controllers
             {
                 return new NotFoundResult();
             }
-            else
-            {
-                var al = (interest / 100) * principle;
-                var ml = ((interest / 100) / 12) * principle;
-                cal_lst.Add(new Calculation() { calId = identity, annInt = al, monInt = ml });
-                return new OkObjectResult(identity);
-            }
+            var al = (interest / 100) * principle;
+            var ml = ((interest / 100) / 12) * principle;
+            cal_lst.Add(new Calculation() { calId = identity, annInt = al, monInt = ml });
+            return new OkObjectResult(identity);
         }
 
         /// <summary>Calculate interest rate for a given principle and interest rate if avaliable</summary>
@@ -64,15 +63,60 @@ namespace interest_calculator.Controllers
             {
                 return new NotFoundResult();
             }
-            else
-            {
-                var al = (interest/100) * principle;
-                var ml = ((interest/100) / 12) * principle;
-                cal_lst.Add(new Calculation() { calId = identity.id, annInt = al, monInt = ml });
-                return new OkObjectResult(cal_lst.Where(o => o.calId == identity.id));
-            }
+            var al = (interest/100) * principle;
+            var ml = ((interest/100) / 12) * principle;
+            cal_lst.Add(new Calculation() { calId = identity.id, annInt = al, monInt = ml });
+            return new OkObjectResult(cal_lst.Where(o => o.calId == identity.id));
         }
 
+        /// <summary>Update the interest rated for the given principle</summary>
+        /// <param principle="principle" interest="interest"></param>
+        /// <returns>updated calculations</returns>
+        [HttpPut("updateInterest/{principle}/{interest}")]
+        public IActionResult updateInterest(float principle, double interest)
+        {
+            var res = GetInterestObject(principle);
+            if (res == null)
+            {
+                return new NotFoundResult();
+            }
+            res.interest = interest;
+            var al = (res.interest / 100) * principle;
+            var ml = ((res.interest / 100) / 12) * principle;
+            cal_lst.Add(new Calculation() { calId = res.id, annInt = al, monInt = ml });
+            return new OkObjectResult(cal_lst.Where(o => o.calId == res.id));
+        }
+
+        /// <summary>Add a new interest and principle object and calculate interest</summary>
+        /// <param principle="principle" interest="interest"></param>
+        /// <returns>calculated interest of the added interest object</returns>
+        [HttpPost("addInterestObj/{principle}/{interest}")]
+        public IActionResult addInterest(float principle, double interest)
+        {
+            int_lst.Add(new Interest() { id=(int_lst.Count+1), principle = principle, interest = interest });
+            var res = int_lst[(int_lst.Count - 1)];
+
+            var al = (res.interest / 100) * principle;
+            var ml = ((res.interest / 100) / 12) * principle;
+            cal_lst.Add(new Calculation() { calId = res.id, annInt = al, monInt = ml });
+            return new OkObjectResult(cal_lst);
+        }
+
+        /// <summary>delete an interest object record with a given principle</summary>
+        /// <param principle="principle"></param>
+        /// <returns>the interest object list without the removed object</returns>
+        [HttpDelete("deleteObject/{principle}")]
+        public IActionResult deleteInterest(float principle)
+        {
+            var res = GetInterestObject(principle);
+            if (res == null)
+            {
+                return new NotFoundResult();
+            }
+
+            int_lst.Remove(res);
+            return new OkObjectResult(int_lst);
+        }
         /// <summary>Obtain the object id for the given principle and interest</summary>
         /// <param principle="principle" interest="interest"></param>
         /// <returns>the Id if present in the list</returns>
@@ -88,6 +132,12 @@ namespace interest_calculator.Controllers
         private Interest GetInterestObject(float principle, double interest)
         {
             var res = int_lst.Where(obj => obj.principle == principle && obj.interest == interest).FirstOrDefault();
+            //var result = cal_lst.Where(o => o.calId == res.id).FirstOrDefault();
+            return res;
+        }
+        private Interest GetInterestObject(float principle)
+        {
+            var res = int_lst.Where(obj => obj.principle == principle).FirstOrDefault();
             //var result = cal_lst.Where(o => o.calId == res.id).FirstOrDefault();
             return res;
         }
